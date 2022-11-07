84630b025307c6e39e360d642a8d3cc4ac02d8bb
Alex-underr <4ubs.pl@gmail.com>
2022-11-07 22:42:21 +0200

    final

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountry } from './fetchCountries.js';
+import countries from './templates/countries.hbs';
 
-const inputEl = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryEl = document.querySelector('.country-info');
-const DEBOUNCE_DELAY = 1000;
+const inputEl = document.querySelector('#search-box');
+const DEBOUNCE_DELAY = 300;
 
 inputEl.addEventListener('input', debounce(handleCountryInput, DEBOUNCE_DELAY));
 
@@ -15,16 +16,53 @@ function handleCountryInput(event) {
   if (!searchQuery) {
     return;
   }
-  fetchCountry(searchQuery).then(console.log).catch(console.log);
+  fetchCountry(searchQuery)
+    .then(resp => {
+      formatInputCountry(resp);
+    })
+    .catch(err => {
+      Notiflix.Notify.failure('Oops, there is no country with that name!');
+    });
 }
 
 function formatInputCountry(countryName) {
-  if (countryName.length <= 10) {
-    Notiflix.Notify.failure('Oops, there is no country with that name!');
-  }
+  countryEl.innerHTML = '';
+  countryList.innerHTML = '';
   if (countryName.length > 10) {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
+    return;
   }
+
+  if (countryName.length <= 10 && countryName.length >= 2) {
+    const markup = countries(countryName);
+    countryList.innerHTML = markup;
+  }
+
+  if (countryName.length === 1) {
+    const markup = createMarkup(countryName);
+    countryEl.innerHTML = markup;
+    return;
+  }
+}
+
+function createMarkup(data) {
+  return data
+    .map(el => {
+      return `<div class='country_inform'>
+  <img class='country_flag' src='${el.flags.svg}' alt='${el.name.official}' />
+  <h1 class='country_name'>${el.name.common}</h1>
+</div>
+<h2 class='country_detail'><span class='header'>Capital: </span>${
+        el.capital
+      }</h2>
+<h2 class='country_detail'><span class='header'>Population: </span>${
+        el.population
+      }</h2>
+<h2 class='country_detail'><span class='header'>Languages: </span>${Object.values(
+        el.languages
+      ).join(', ')}</h2>`;
+    })
+    .join();
 }
